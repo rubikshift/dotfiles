777cadb4f5339a869fd39faad4a8480ce41c160b
Michal Krakowiak <michal.krak97@gmail.com>
2021-02-07 12:57:13 +0100

    fix fish lang variable

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,13 @@
+set -gx LANG "en_US.UTF-8"
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_date_format "+ %H:%M"
 set -g theme_title_use_abbreviated_path no
-set -g theme_color_scheme dracula
+set -g theme_color_scheme nord
 set -g theme_nerd_fonts no
+
 set -g theme_newline_cursor yes
+
+# set --universal ayu_variant dark && ayu_load_theme
 
 alias vi=nvim
 alias vim=nvim
@@ -11,7 +15,6 @@ alias vim=nvim
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
-set -gx LANG en_US
 
 function brewup
     brew update
