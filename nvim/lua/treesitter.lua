097096d6173df251978272fdbd2f05b4ef222865
Michal Krakowiak <michal.krak97@gmail.com>
2021-09-05 15:52:45 +0200

    ignore haskell in treesitter

--- nvim/lua/treesitter.lua
+++ nvim/lua/treesitter.lua
@@ -1,4 +1,5 @@
 require'nvim-treesitter.configs'.setup {
+  ignore_install = {"haskell"},
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,              -- false will disable the whole extension
