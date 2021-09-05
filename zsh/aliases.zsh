9978e692b0b26261f3dbac58082052424df9678f
Michal Krakowiak <michal.krakowiak@oracle.com>
2021-09-05 15:35:41 +0200

    added aliases.zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,11 @@
+alias vi=nvim
+alias vim=nvim
+alias cat=bat
+alias ls="exa --icons --color=always"
+alias ldo=lazydocker
+alias lg=lazygit
+alias les=less
+alias rgf="rg --files | rg -i"
+alias find=fd
+alias fl="l | rg -i"
+alias brewup="brew update && brew upgrade; brew doctor"
