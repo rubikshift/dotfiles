d9bae1b2e632053fbd6171de3828190574d11ae1
Michal Krakowiak <michal.krakowiak@oracle.com>
2021-09-05 15:46:58 +0200

    added exports.zsh

---
+++ zsh/exports.zsh
@@ -0,0 +1,6 @@
+# export PATH="/usr/local/opt/python/libexec/bin:$HOME/bin:$HOME/.local/bin:/usr/local/bin:/usr/bin:$PATH"
+export PATH="$PATH:/usr/local/opt/llvm/bin"
+export LC_ALL=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export VISUAL=nvim
+export EDITOR="$VISUAL"
