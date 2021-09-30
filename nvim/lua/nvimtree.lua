80ab5048e2c8778c2146d93b696b3eeff39c565d
Michal Krakowiak <michal.krakowiak@oracle.com>
2021-09-30 09:57:27 +0200

    changing nvim tree setup due to plugin update

--- nvim/lua/nvimtree.lua
+++ nvim/lua/nvimtree.lua
@@ -1,5 +1,13 @@
+vim.g.nvim_tree_ignore = { '.git', '.cache' }
 vim.g.nvim_tree_hide_dotfiles = 1
 vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_follow = 1
-vim.g.nvim_tree_auto_close = 1
+
+require('nvim-tree').setup({
+  hijack_cursor = true,
+  tree_auto_close = true,
+  update_focused_file = {
+    enable = true,
+  },
+})
+
 
