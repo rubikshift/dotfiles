e449a73e28ba46aeb6057f676521eb1654d8d48e
Michal Krakowiak <michal.krakowiak@oracle.com>
2021-09-30 09:36:48 +0200

    another files to my zsh setup

---
+++ zsh/plugins.zsh
@@ -0,0 +1,3 @@
+source "$HOME/repos/dotfiles/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "$HOME/repos/dotfiles/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "$HOME/repos/dotfiles/zsh/zsh-history-substring-search/zsh-history-substring-search.zsh"
--- zsh/zcompinit.zsh
+++ zsh/zcompinit.zsh
@@ -1,4 +1,10 @@
 autoload -Uz compinit
+
+# completions
+fpath=("$HOME/repos/dotfiles/zsh/zsh-completions/src" $fpath)
+zmodload zsh/complist
+_comp_options+=(globdots)	
+
 if [ $(date +'%j') != $(/usr/bin/stat -f '%Sm' -t '%j' ${ZDOTDIR:-$HOME}/.zcompdump) ]; then
   compinit
 else
