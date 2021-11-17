882f7613b0ea0a07854b0e74fdba5f231d6f999b
Michal Krakowiak <michal.krakowiak@oracle.com>
2021-11-17 16:45:22 +0100

    formatting, minimum keyword length for buffer completion

--- nvim/lua/config/nvim-cmp.lua
+++ nvim/lua/config/nvim-cmp.lua
@@ -16,38 +16,38 @@ cmp.setup({
     completion = {
         completeopt = 'menu,menuone,noinsert',
     },
-mapping = {
-    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i', 'c'}),
-    ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i', 'c'}),
-    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
-    ['<C-e>'] = cmp.mapping.close({
-        i = cmp.mapping.abort(),
-        c = cmp.mapping.close(),
-    }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-},
-sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'treesiter' },
-    { name = 'buffer' },
-    { name = 'path' },
-    { name = 'cmdline' }
-},
-formatting = {
-    format = lspkind.cmp_format(),
-},
-experimental = {
-    -- native_menu = true,
-    ghost_text = true,
-}
+    mapping = {
+        ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i', 'c'}),
+        ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i', 'c'}),
+        ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
+        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
+        ['<C-e>'] = cmp.mapping.close({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'treesiter' },
+        { name = 'buffer', keyword_length=5 },
+        { name = 'path' },
+        -- { name = 'cmdline' }
+    },
+    formatting = {
+        format = lspkind.cmp_format(),
+    },
+    experimental = {
+        -- native_menu = true,
+        -- ghost_text = true,
+    }
 })
 
 cmp.setup.cmdline('/', {
     sources = {
-        { name = 'buffer' }
+        { name = 'buffer', keyword_length=3 }
     }
 })
 
