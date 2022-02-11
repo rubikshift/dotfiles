c9b91f48e033b3896072f7078c526ae5ba40cb26
Michal Krakowiak <michal.krak97@gmail.com>
2022-02-11 18:15:16 +0100

    updated zoxide

--- zsh/zoxide.zsh
+++ zsh/zoxide.zsh
@@ -21,13 +21,13 @@ function __zoxide_cd() {
 
 # Hook to add new entries to the database.
 function __zoxide_hook() {
-    zoxide add -- "$(__zoxide_pwd)"
+    \command zoxide add -- "$(__zoxide_pwd || \builtin true)"
 }
 
 # Initialize hook.
-if [ "${__zoxide_hooked}" != '1' ]; then
-    __zoxide_hooked='1'
-    chpwd_functions=("${chpwd_functions[@]}" "__zoxide_hook")
+# shellcheck disable=SC2154
+if [[ ${precmd_functions[(Ie)__zoxide_hook]:-} -eq 0 ]] && [[ ${chpwd_functions[(Ie)__zoxide_hook]:-} -eq 0 ]]; then
+    chpwd_functions+=(__zoxide_hook)
 fi
 
 # =============================================================================
@@ -36,31 +36,38 @@ fi
 # desired.
 #
 
+__zoxide_z_prefix='z#'
+
 # Jump to a directory using only keywords.
 function __zoxide_z() {
-    if [ "$#" -eq 0 ]; then
+    # shellcheck disable=SC2199
+    if [[ "$#" -eq 0 ]]; then
         __zoxide_cd ~
-    elif [ "$#" -eq 1 ] && [ "$1" = '-' ]; then
-        if [ -n "${OLDPWD}" ]; then
+    elif [[ "$#" -eq 1 ]] && [[ "$1" = '-' ]]; then
+        if [[ -n "${OLDPWD}" ]]; then
             __zoxide_cd "${OLDPWD}"
         else
             # shellcheck disable=SC2016
             \builtin printf 'zoxide: $OLDPWD is not set'
             return 1
         fi
-    elif [ "$#" -eq 1 ] &&  [ -d "$1" ]; then
+    elif [[ "$#" -eq 1 ]] && [[ -d "$1" ]]; then
         __zoxide_cd "$1"
+    elif [[ "$@[-1]" == "${__zoxide_z_prefix}"* ]]; then
+        # shellcheck disable=SC2124
+        \builtin local result="${@[-1]}"
+        __zoxide_cd "${result:${#__zoxide_z_prefix}}"
     else
-        \builtin local __zoxide_result
-        __zoxide_result="$(zoxide query --exclude "$(__zoxide_pwd)" -- "$@")" \
-            && __zoxide_cd "${__zoxide_result}"
+        \builtin local result
+        result="$(\command zoxide query --exclude "$(__zoxide_pwd || \builtin true)" -- "$@")" &&
+            __zoxide_cd "${result}"
     fi
 }
 
 # Jump to a directory using interactive search.
 function __zoxide_zi() {
-    \builtin local __zoxide_result
-    __zoxide_result="$(zoxide query -i -- "$@")" && __zoxide_cd "${__zoxide_result}"
+    \builtin local result
+    result="$(\command zoxide query -i -- "$@")" && __zoxide_cd "${result}"
 }
 
 # =============================================================================
@@ -70,20 +77,57 @@ function __zoxide_zi() {
 
 # Remove definitions.
 function __zoxide_unset() {
-    \builtin unalias "$@" &>/dev/null
-    \builtin unfunction "$@" &>/dev/null
+    \builtin unalias "$@" &>/dev/null || \builtin true
+    \builtin unfunction "$@" &>/dev/null || \builtin true
     \builtin unset "$@" &>/dev/null
 }
 
-__zoxide_unset 'z'
+__zoxide_unset z
 function z() {
     __zoxide_z "$@"
 }
 
-__zoxide_unset 'zi'
+__zoxide_unset zi
 function zi() {
     __zoxide_zi "$@"
 }
+
+if [[ -o zle ]]; then
+    __zoxide_unset _z
+    function _z() {
+        # Only show completions when the cursor is at the end of the line.
+        # shellcheck disable=SC2154
+        [[ "${#words[@]}" -eq "${CURRENT}" ]] || return
+
+        if [[ "${#words[@]}" -eq 2 ]]; then
+            _files -/
+        elif [[ "${words[-1]}" == '' ]]; then
+            \builtin local result
+            # shellcheck disable=SC2086
+            if result="$(\command zoxide query -i -- ${words[2,-1]})"; then
+                __zoxide_result="${result}"
+            else
+                __zoxide_result=''
+            fi
+            \builtin printf '\e[5n'
+        fi
+    }
+
+    __zoxide_unset _z_helper
+    function _z_helper() {
+        \builtin local result="${__zoxide_z_prefix}${__zoxide_result}"
+        # shellcheck disable=SC2296
+        [[ -n "${__zoxide_result}" ]] && LBUFFER="${LBUFFER}${(q-)result}"
+        \builtin zle reset-prompt
+    }
+
+    \builtin zle -N _z_helper
+    \builtin bindkey "\e[0n" _z_helper
+    if [[ "${+functions[compdef]}" -ne 0 ]]; then
+        \compdef -d z
+        \compdef _z z
+    fi
+fi
 
 # =============================================================================
 #
