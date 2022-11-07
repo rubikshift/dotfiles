8bbf325e3ff96cdddea866ea147ad3d7cad00416
Michal Krakowiak <michal.krakowiak@oracle.com>
2022-11-07 13:03:36 +0100

    removed trailing spaces

--- zsh/zcompinit.zsh
+++ zsh/zcompinit.zsh
@@ -3,7 +3,7 @@ autoload -Uz compinit
 # completions
 fpath=("$HOME/repos/dotfiles/zsh/zsh-completions/src" $fpath)
 zmodload zsh/complist
-_comp_options+=(globdots)	
+_comp_options+=(globdots)
 
 if [ $(date +'%j') != $(/usr/bin/stat -f '%Sm' -t '%j' ${ZDOTDIR:-$HOME}/.zcompdump) ]; then
   compinit
