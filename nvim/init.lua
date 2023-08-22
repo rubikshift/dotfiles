d4028decb6ef6a11d07b142baa76e160ff16de74
Michal Krakowiak <michal.krakowiak@oracle.com>
2023-08-22 15:08:05 +0200

    init.lua update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,22 @@
 -- inspired by https://github.com/ChristianChiarulli/nvcode
 -- other cool stuff https://github.com/rockerBOO/awesome-neovim#treesitter-support
 
-require('impatient')
+if vim.g.vscode then
+else
+    require('impatient')
 
-require('settings').load_options()
-require('keymap')
+    require('settings').load_options()
+    require('keymap')
 
-require('packer_compiled')
-require('plugins')
+    require('packer_compiled')
+    require('plugins')
 
-require('lsp.cmake')
-require('lsp.cpp')
-require('lsp.python')
-require('lsp.r')
-require('lsp.ltex-ls')
-require('lsp.texlab')
+    require('lsp.cmake')
+    require('lsp.cpp')
+    require('lsp.python')
+    require('lsp.r')
+    require('lsp.ltex-ls')
+    require('lsp.texlab')
+    require('lsp.groovy')
+end
+
