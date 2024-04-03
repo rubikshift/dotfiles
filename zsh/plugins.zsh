0c9cd5bb499472dfb2067e3b4b63b4e36ef143c9
Michal Krakowiak <michal.krakowiak@intel.com>
2024-04-03 15:06:57 +0200

    ctrl+r working again

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -2,6 +2,8 @@ source "$HOME/repos/dotfiles/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
 source "$HOME/repos/dotfiles/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 source "$HOME/repos/dotfiles/zsh/zsh-history-substring-search/zsh-history-substring-search.zsh"
 
+bindkey -v
+bindkey '^R' history-incremental-search-backward
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 bindkey -M vicmd 'k' history-substring-search-up
