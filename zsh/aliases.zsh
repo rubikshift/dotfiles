0ad3246aa2bbd6b281f0f134be1336f4e6fde42a
Michal Krakowiak <michal.krakowiak@intel.com>
2024-04-15 09:13:27 +0200

    zsh: refine settings

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,12 +1,12 @@
-alias brewup="brew update && brew upgrade; brew doctor"
-alias cat=bat
-alias docker=podman
-alias find=fd
-alias fl="l | rg -i"
+command -v brew > /dev/null && alias brewup="brew update && brew upgrade; brew doctor"
+command -v bat > /dev/null && alias cat=bat || command -v batcat > /dev/null && alias cat=batcat
+# alias docker=podman
+command -v fd > /dev/null alias find=fd || command -v fdfind > /dev/null alias find=fdfind
 alias l="ls -lah"
+alias fl="l | rg -i"
 alias les=less
 alias lg=lazygit
-alias ls="exa --color=always"
+command -v eza > /dev/null && alias ls="eza -g --color=always"
 alias mail=neomutt
 alias r="python3 -m radian"
 alias rgf="rg --files | rg -i"
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,10 +1,10 @@
-source "$HOME/repos/dotfiles/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
-source "$HOME/repos/dotfiles/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-source "$HOME/repos/dotfiles/zsh/zsh-history-substring-search/zsh-history-substring-search.zsh"
+source "$XDG_CONFIG_HOME/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "$XDG_CONFIG_HOME/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "$XDG_CONFIG_HOME/zsh/zsh-history-substring-search/zsh-history-substring-search.zsh"
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-bindkey -v
-bindkey '^R' history-incremental-search-backward
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 bindkey -M vicmd 'k' history-substring-search-up
 bindkey -M vicmd 'j' history-substring-search-down
+bindkey "^[[3~" delete-char
