c5768fbf17ab151198f3668bce2fb3b8311cf414
Michal Krakowiak <michal.krakowiak@intel.com>
2024-05-16 16:31:08 +0200

    fix aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,6 @@
 command -v brew > /dev/null && alias brewup="brew update && brew upgrade; brew doctor"
-command -v bat > /dev/null && alias cat=bat || command -v batcat > /dev/null && alias cat=batcat
-# alias docker=podman
-command -v fd > /dev/null && alias find=fd || command -v fdfind > /dev/null && alias find=fdfind
+{ command -v bat > /dev/null && alias cat=bat } || { command -v batcat > /dev/null && alias cat=batcat }
+{ command -v fd > /dev/null && alias find=fd } || { command -v fdfind > /dev/null && alias find=fdfind && alias fd=fdfind}
 alias l="ls -lah"
 alias fl="l | rg -i"
 alias les=less
