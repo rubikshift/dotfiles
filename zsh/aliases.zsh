bd1da3e5026946f9c86109836248c4deb55ed0f4
Michal Krakowiak <michal.krakowiak@intel.com>
2024-05-16 16:31:23 +0200

    ls/sl alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,11 +1,12 @@
 command -v brew > /dev/null && alias brewup="brew update && brew upgrade; brew doctor"
 { command -v bat > /dev/null && alias cat=bat } || { command -v batcat > /dev/null && alias cat=batcat }
 { command -v fd > /dev/null && alias find=fd } || { command -v fdfind > /dev/null && alias find=fdfind && alias fd=fdfind}
+command -v eza > /dev/null && alias ls="eza -g --color=always"
+alias sl=ls
 alias l="ls -lah"
 alias fl="l | rg -i"
 alias les=less
 alias lg=lazygit
-command -v eza > /dev/null && alias ls="eza -g --color=always"
 alias mail=neomutt
 alias r="python3 -m radian"
 alias rgf="rg --files | rg -i"
